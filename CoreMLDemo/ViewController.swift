d0c3bf28d46e921043e8bffa466e47c71e5e9df6
csomet <csomet@me.com>
2018-03-29 17:44:16 +0200

    Initial Commit

---
+++ CoreMLDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLDemo
+//
+//  Created by Carlos Herrera Somet on 29/3/18.
+//  Copyright © 2018 Carlos H Somet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
